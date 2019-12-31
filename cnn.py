5c215f485f0aa89aa09c9e5aec201d345ea837ef
rsongdalsong <jeongwon412@gmail.com>
2019-12-31 01:46:01 -0500

    train n2000e1b32.h5

--- cnn.py
+++ cnn.py
@@ -1,31 +1,43 @@
+# Model
+# save https://www.tensorflow.org/guide/keras/save_and_serialize
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
 
 IMG_SHAPE = (128, 128)
 BATCH_SIZE = 32
-NUM_DATA = 14034
+
+NUM_DATA = 2000 
+NUM_TEST = 200
+EPOCHS = 1
+PATH = 'intel-image-classification/'
+PATH_TRAIN = PATH + 'seg_train/seg_train'
+PATH_TEST = PATH + 'seg_test/seg_test'
+
+
+"""
+Ubuntu NVIDA => sad life
+NUM_DATA = 14034 
 NUM_TEST = 3000
+EPOCHS = 50
+"""
+
 # data preprocess
 train_idg = ImageDataGenerator(rescale = 1./255, shear_range = 0.2, zoom_range = 0.2, horizontal_flip = True)
 test_idg = ImageDataGenerator( rescale = 1./255,)
 
 train_gen = train_idg.flow_from_directory(
-        'intel-image-classification/seg_train/seg_train',
+        PATH_TRAIN,
         target_size = IMG_SHAPE,
         batch_size = BATCH_SIZE,
         class_mode = 'categorical')
 
 test_gen = test_idg.flow_from_directory(
-        'intel-image-classification/seg_test/seg_test',
+        PATH_TEST,
         target_size = IMG_SHAPE,
         batch_size = BATCH_SIZE,
         class_mode='categorical')
 
-
-# Model
-# save https://www.tensorflow.org/guide/keras/save_and_serialize
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
 
 model = Sequential()
 
@@ -43,7 +55,12 @@ model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accur
 
 model.fit_generator(
         generator=train_gen, 
-        steps_per_epoch=NUM_DATA//BATCH_SIZE, 
+        steps_per_epoch=NUM_DATA, 
         validation_data=test_gen,
-        validation_steps=NUM_TEST//BATCH_SIZE,
-        epochs=50)
+        validation_steps=NUM_TEST,
+        epochs=EPOCHS)
+
+
+loss, evaluate = model.evaluate_generator(test_gen, steps=NUM_TEST)
+# loss = 0.5321 & evaluate = 0.8156
+# model.save('n2000e1b32.h5')
